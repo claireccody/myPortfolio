2b383c1ddea35840be8e14221d9a0ddfcd103f6d
claireccody <ccody_uo@hotmail.com>
2026-05-06 11:50:05 -0700

    First commit for initialization of repo

---
+++ dynamics.js
@@ -0,0 +1,61 @@
+function galleryScroll() {
+  console.log("galleryScroll");
+  let currGal = document.getElementById("Gallery").value;
+  console.log(currGal);
+
+  let galOpts = document.querySelectorAll(".gallery-opt");
+
+  if (currGal == "") {
+    galOpts.forEach((opt) => {
+      if (opt.classList.contains("hide") == true) {
+        opt.classList.toggle("hide");
+        console.log(opt.classList);
+      }
+    });
+  } else {
+    galOpts.forEach((opt) => {
+      if (opt.classList.contains("hide") == false) {
+        opt.classList.add("hide");
+        console.log(opt.classList);
+      }
+    });
+    document.querySelector(`#${currGal}`).classList.toggle("hide");
+  }
+
+  document.querySelector("#gallery").scrollIntoView();
+}
+function galleryScrollB() {
+  console.log("galleryScroll");
+  let currGal = document.getElementById("GalleryB").value;
+  console.log(currGal);
+
+  let galOpts = document.querySelectorAll(".gallery-opt");
+
+  if (currGal == "") {
+    galOpts.forEach((opt) => {
+      if (opt.classList.contains("hide") == true) {
+        opt.classList.toggle("hide");
+        console.log(opt.classList);
+      }
+    });
+  } else {
+    galOpts.forEach((opt) => {
+      if (opt.classList.contains("hide") == false) {
+        opt.classList.add("hide");
+        console.log(opt.classList);
+      }
+    });
+    document.querySelector(`#${currGal}`).classList.toggle("hide");
+  }
+
+  document.querySelector("#gallery").scrollIntoView();
+}
+
+function viewCAT() {
+  document.getElementById("cat_pack").classList.toggle("hide");
+}
+
+function viewKFP() {
+  document.getElementById("kfp_pack").classList.toggle("hide");
+
+}
